1277d3964aa3b6e9270de435006fb58d9a590a95
Ji <jjeonie@gmail.com>
2020-04-13 10:08:28 +1200

    Format files and update workflow configuration file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-      "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
-    }
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
