cd757d94dcb8cc70304bb52333b20c6614d5db84
Will Molloy <willjoemolloy@gmail.com>
2020-06-04 00:30:30 +1200

    Switch eslint to ecma 2019 since node v12 is used

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "ecmaVersion": 2020,
+    "ecmaVersion": 2019,
     "sourceType": "module",
     "ecmaFeatures": {
       "jsx": true
