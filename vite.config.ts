0308e3bd2e8dde89b4d7b66d4feaba8b75e164b4
Will Molloy <willjoemolloy@gmail.com>
2024-06-25 07:55:56 +1200

    Add code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-// https://vitest.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://vitest.dev/config/
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+    },
   },
 });
