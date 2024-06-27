609779673b4e7ed23ac177b0da06de1a65d014e0
Will Molloy <willjoemolloy@gmail.com>
2024-06-27 12:42:39 +1200

    Refactor tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import App from './App';
 
-describe('App Component', () => {
-  test('renders Vite and React logos with links', () => {
+describe('[components] App', () => {
+  it('renders Vite and React logos with links', () => {
     render(<App />);
     const viteLogo = screen.getByAltText('Vite logo');
     const reactLogo = screen.getByAltText('React logo');
@@ -11,13 +11,13 @@ describe('App Component', () => {
     expect(reactLogo).toBeInTheDocument();
   });
 
-  test('renders the heading', () => {
+  it('renders the heading', () => {
     render(<App />);
     const heading = screen.getByText('Vite + React');
     expect(heading).toBeInTheDocument();
   });
 
-  test('renders the button and increments count on click', () => {
+  it('renders the button and increments count on click', () => {
     render(<App />);
     const button = screen.getByText('count is 0');
     expect(button).toBeInTheDocument();
@@ -25,14 +25,14 @@ describe('App Component', () => {
     expect(button).toHaveTextContent('count is 1');
   });
 
-  test('renders the instruction text', () => {
+  it('renders the instruction text', () => {
     render(<App />);
     const instructionText = screen.getByText('Edit ', { exact: false });
     expect(instructionText.textContent).toEqual('Edit src/App.tsx and save to test HMR');
     expect(instructionText).toBeInTheDocument();
   });
 
-  test('renders the footer text', () => {
+  it('renders the footer text', () => {
     render(<App />);
     const footerText = screen.getByText('Click on the Vite and React logos to learn more');
     expect(footerText).toBeInTheDocument();
